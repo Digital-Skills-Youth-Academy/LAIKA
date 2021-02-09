8b1723ac68b2d9033a076522ef88751f90f85c28
William Henke <william.henke@computingforall.org>
2021-02-08 17:43:32 -0800

    working on js and css

---
+++ script.js
@@ -0,0 +1,11 @@
+// var submitButton1 = document.getElementById('submitButton1').addEventListener("click", comment1);
+
+// function comment1() {
+//     event.preventDefault();
+//         let saveComment = {
+//             comment1: document.getElementById('inputField1').value,
+//         }
+//         localStorage.setItem('comment1', JSON.stringify(saveComment));
+//         IDArray.push(saveUserIds);
+//     }
+// }
