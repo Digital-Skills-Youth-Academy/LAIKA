1c1bb586f50636564e1fb301214e2aa7250e4f6c
William Henke <william.henke@computingforall.org>
2021-02-17 18:38:44 -0800

    a little progress on the description editing feature

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
 let submitButton1 = document.getElementById('submitButton1').addEventListener("click", comment1);
-let imageComment1 = document.getElementById('imageComment1');
 let inputField1 = document.getElementById('inputField1');
 
 let submitButton2 = document.getElementById('submitButton2').addEventListener("click", comment2);
-let imageComment2 = document.getElementById('imageComment2');
 let inputField2 = document.getElementById('inputField2');
 
 let submitButton3 = document.getElementById('submitButton3').addEventListener("click", comment3);
-let imageComment3 = document.getElementById('imageComment3');
 let inputField3 = document.getElementById('inputField3');
 
 let addImage = document.getElementById('addImage');
 
 function comment1() {
     event.preventDefault();
-    imageComment1.innerHTML = inputField1.value;
+    document.getElementById('imageDescription1').innerHTML = inputField1.value;
     inputField1.value = "";
 }
 
 function comment2() {
     event.preventDefault();
-    imageComment2.innerHTML = inputField2.value;
+    document.getElementById('imageDescription2').innerHTML = inputField2.value;
     inputField2.value = "";
 }
 
 function comment3() {
     event.preventDefault();
-    imageComment3.innerHTML = inputField3.value;
+    document.getElementById('imageDescription3').innerHTML = inputField3.value;
     inputField3.value = "";
+}
+
+let description1 = document.getElementById('imageDescription1').addEventListener("click", editDescription)
+
+function editDescription() {
+
 }
